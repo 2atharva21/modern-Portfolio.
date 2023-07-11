a388b8ee4aca3660928e4efae930fd8d079db758
Atharva zare <120238220+2atharva21@users.noreply.github.com>
2023-07-11 08:02:34 +0530

    Add files via upload

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+
+import Wrapper from "./Wrapper";
+import flag from "../assets/indian-flag.png";
+import pe1 from "../assets/sec-2-p-e-1.png";
+import pe2 from "../assets/sec-2-p-e-2.png";
+import Div from "./Div";
+
+const AboutMe = () => {
+    const { scrollY } = useScroll();
+    const y1 = useTransform(scrollY, [0, 500], [100, 0], { clamp: false });
+    const y2 = useTransform(scrollY, [0, 500], [0, -200], { clamp: false });
+    return (
+        <div
+            id="about"
+            className="w-full py-[50px] md:py-[100px] bg-white text-black relative overflow-hidden"
+        >
+            {/* BACKGROUND ELEMENTS START */}
+            <span className="sec-2-bg-gradient" />
+            <motion.img className="sec-2-p-e-1" style={{ y: y1 }} src={pe1} />
+            <motion.img className="sec-2-p-e-2" style={{ y: y2 }} src={pe2} />
+            {/* BACKGROUND ELEMENTS END */}
+
+            <Wrapper>
+                {/* HEADING START */}
+                <Div className="text-[40px] md:text-[90px] 2xl:text-[120px] leading-[52px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase mb-[25px] md:mb-[40px] 2xl:mb-[60px] flex flex-col relative">
+                    <span>A Product Designer</span>
+                    <span className="flex items-center gap-2">
+                        <span>Based</span>
+                        <img
+                            src={flag}
+                            alt=""
+                            className="w-[70px] md:w-[150px] 2xl:w-[190px] block mt-1 md:mt-2"
+                        />
+                        <span>In India</span>
+                    </span>
+                </Div>
+                {/* HEADING END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                Complex Web Design Project:
+The portfolio highlights the successful completion of a challenging web design project that required meticulous attention to detail, a deep understanding of cross-browser compatibility, and the implementation of cutting-edge design practices. This achievement demonstrates the developer's ability to tackle complex requirements and deliver high-quality results.
+
+
+                </Div>
+                {/* PARAGRAPH END */}
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                Mobile-First Web Application:
+A significant achievement showcased in the portfolio is the development of a mobile-first web application using a frontend framework. The project's success is highlighted by metrics showing improved user engagement and conversion rates. This achievement demonstrates the developer's ability to create seamless mobile experiences and deliver tangible business value. </Div>
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                Open-Source Contributions:
+The developer's contributions to open-source projects or personal projects that have gained recognition within the developer community are prominently featured. These contributions demonstrate a commitment to collaboration, knowledge-sharing, and continuous learning, showcasing the developer's active involvement in the broader developer ecosystem.
+                </Div>
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] relative">
+                Performance Optimization Strategy:
+The portfolio presents an achievement in implementing a performance optimization strategy for a web project, resulting in a substantial reduction in page load time. This accomplishment demonstrates the developer's expertise in optimizing website performance through techniques like code minification, image optimization, and caching, leading to enhanced user experiences and improved search engine rankings.
+                    
+                </Div>
+                {/* PARAGRAPH END */}
+            </Wrapper>
+        </div>
+    );
+};
+
+export default AboutMe;
